01f4b69a6072417d30dd328a51b7596271e6c0ea
Ray <qc@pop-os.localdomain>
2021-07-10 18:34:27 +0530

    mobile partiallty fixed

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
 
-function toggleNav() {
-    if (document.getElementById('mySidenav').style.transform === 'translate(0px, 0px)') {
-        document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)';
-    } else {
-        document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+
+if(window.innerWidth < 786){
+    function toggleNav() {
+        if (document.getElementById('mySidenav').style.transform === 'translate(0px, 0px)') {
+            document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)'
+            document.getElementById('#tog-arrow').style.transform = 'rotate(0deg)';
+        } else {
+            document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+        }
     }
 }
+
