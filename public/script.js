9e98a88ddb3116dee79ffe259faf78c825d96279
Ray <qc@pop-os.localdomain>
2021-07-10 22:02:31 +0530

    some improvement

--- public/script.js
+++ public/script.js
@@ -5,11 +5,16 @@
         if(window.innerWidth < 786){
             if (document.getElementById('mySidenav').style.transform === 'translate(0px, 0px)') {
                 document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)'
+                document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)' 
             } else {
                 document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+                document.getElementById('mySidenav').style.visibility= 'visible';
+                
             }
         }
-       
+        else if(document.getElementById('mySidenav').style.transform = 'translate(-100%, 0px)'){
+            document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+        }
         
     }
 
