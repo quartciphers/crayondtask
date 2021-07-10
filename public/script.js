4eec73be73ef9ebdfa9f84a3f5ff645aca8dc033
Ray <qc@pop-os.localdomain>
2021-07-10 18:45:58 +0530

    js fix

--- public/script.js
+++ public/script.js
@@ -1,13 +1,16 @@
 
 
-if(window.innerWidth < 786){
     function toggleNav() {
-        if (document.getElementById('mySidenav').style.transform === 'translate(0px, 0px)') {
-            document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)'
-            document.getElementById('#tog-arrow').style.transform = 'rotate(0deg)';
-        } else {
-            document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+       
+        if(window.innerWidth < 786){
+            if (document.getElementById('mySidenav').style.transform === 'translate(0px, 0px)') {
+                document.getElementById('mySidenav').style.transform = 'translate(-100%, 0)'
+            } else {
+                document.getElementById('mySidenav').style.transform = 'translate(0px, 0px)';
+            }
         }
+       
+        
     }
-}
+
 
